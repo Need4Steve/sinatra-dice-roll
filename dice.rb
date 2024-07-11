46f0ed6477657e1de488423039387b5d25463387
Stephen Schneider <stephenschneider@Stephens-MacBook-Pro-2.local>
2024-07-11 17:27:59 -0500

    added better error handlers

--- dice.rb
+++ dice.rb
@@ -1,5 +1,11 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   "Hello World"
@@ -11,4 +17,15 @@ end
 
 get("/giraffe") do
   "Hopefully this shows up without having to restart the server :0"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+
+  "<h1>2d6</h1>
+  <p>#{outcome}</p>"
 end
