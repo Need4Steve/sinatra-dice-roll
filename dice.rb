9a6c99e2ba6ca1612ab7d0c57279068d36f06fd7
Stephen Schneider <stephenschneider@Stephens-MacBook-Pro-2.local>
2024-07-11 18:34:51 -0500

    started views

--- dice.rb
+++ dice.rb
@@ -8,16 +8,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Dice Roll</h1>
-	
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>
-  "
+  erb(:elephant)
 end
 
 get("/zebra") do
@@ -28,9 +19,9 @@ get("/giraffe") do
   "Hopefully this shows up without having to restart the server :0"
 end
 
-get("/dice/2/6") do
-  first_die = rand(1..6)aa
-  second_die = rand(1..6)
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
   sum = first_die + second_die
 
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
@@ -39,27 +30,29 @@ get("/dice/2/6") do
   <p>#{outcome}</p>"
 end
 
-get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+get("/dice/1/20") do
+  first_die = rand(1..20)
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+  outcome = "You rolled a #{first_die} for a total of #{first_die}"
 
   "<h1>2d6</h1>
   <p>#{outcome}</p>"
 end
 
-get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}"
 
   "<h1>2d6</h1>
   <p>#{outcome}</p>"
 end
+
 get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
