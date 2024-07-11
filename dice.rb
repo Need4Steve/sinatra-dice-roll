b82489474430b9f2c78b6e482f4e3b41e212fa8f
Stephen Schneider <stephenschneider@Stephens-MacBook-Pro-2.local>
2024-07-11 17:35:23 -0500

    starting other dice pages

--- dice.rb
+++ dice.rb
@@ -8,7 +8,16 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "Hello World"
+  "
+  <h1>Dice Roll</h1>
+	
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  </ul>
+  "
 end
 
 get("/zebra") do
@@ -19,6 +28,38 @@ get("/giraffe") do
   "Hopefully this shows up without having to restart the server :0"
 end
 
+get("/dice/2/6") do
+  first_die = rand(1..6)aa
+  second_die = rand(1..6)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+
+  "<h1>2d6</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+
+  "<h1>2d6</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+
+  "<h1>2d6</h1>
+  <p>#{outcome}</p>"
+end
 get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
